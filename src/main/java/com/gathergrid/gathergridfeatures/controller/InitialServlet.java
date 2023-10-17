642bcca915d448c25a324782a79ae2b4bbd44fc9
Mehdi Qaos <mehdi.qaos@gmail.com>
2023-10-17 10:22:03 +0100

    added EntityManagerUtil

--- src/main/java/com/gathergrid/gathergridfeatures/controller/InitialServlet.java
+++ src/main/java/com/gathergrid/gathergridfeatures/controller/InitialServlet.java
@@ -3,9 +3,10 @@ package com.gathergrid.gathergridfeatures.controller;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(loadOnStartup = 1)
-public class InitialServlet extends HelloServlet {
+public class InitialServlet extends HttpServlet {
     @Override
     public void init() {
         EntityManagerFactory efm = Persistence.createEntityManagerFactory("my-persistence-unit");
