1bd2d06972d9c09d8eba30d612d7617dcf2bffa7
Mehdi Qaos <mehdi.qaos@gmail.com>
2023-10-17 11:16:24 +0100

    added EventRepositoty

--- src/main/java/com/gathergrid/gathergridfeatures/repository/interfaces/EventRepository.java
+++ src/main/java/com/gathergrid/gathergridfeatures/repository/interfaces/EventRepository.java
@@ -1,4 +1,13 @@
 package com.gathergrid.gathergridfeatures.repository.interfaces;
 
+import com.gathergrid.gathergridfeatures.domain.Event;
+
+import java.util.List;
+
 public interface EventRepository {
+    public Event createEvent(Event event);
+    public void deleteEvent(long id);
+    public void updateEvent(Event event);
+    public Event find(long id);
+    public List<Event> findAll();
 }
--- src/main/java/com/gathergrid/gathergridfeatures/repository/interfacesImpl/EventRepositoryImpl.java
+++ src/main/java/com/gathergrid/gathergridfeatures/repository/interfacesImpl/EventRepositoryImpl.java
@@ -1,4 +1,53 @@
 package com.gathergrid.gathergridfeatures.repository.interfacesImpl;
 
-public class EventRepositoryImpl {
+import com.gathergrid.gathergridfeatures.domain.Event;
+import com.gathergrid.gathergridfeatures.repository.interfaces.EventRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class EventRepositoryImpl implements EventRepository {
+    private final EntityManager em;
+
+    public EventRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Event createEvent(Event event) {
+        em.getTransaction().begin();
+        em.persist(event);
+        em.getTransaction().commit();
+        return event;
+    }
+
+    @Override
+    public void deleteEvent(long id) {
+        em.getTransaction().begin();
+        Event event = em.find(Event.class, id);
+        if (event != null) {
+            em.remove(event);
+        }
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public void updateEvent(Event event) {
+        em.getTransaction().begin();
+        em.merge(event);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public Event find(long id) {
+        return em.find(Event.class, id);
+    }
+
+    @Override
+    public List<Event> findAll() {
+        String jpql = "SELECT e FROM Event e";
+        TypedQuery<Event> query = em.createQuery(jpql, Event.class);
+        return query.getResultList();
+    }
 }
