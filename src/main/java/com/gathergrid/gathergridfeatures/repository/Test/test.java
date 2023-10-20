e4015f9fa7399858d77cfef0278b3b30b498faee
moussafia <moussafia.mohammed1999@gmail.com>
2023-10-20 12:24:32 +0100

    remove methode import from test

--- src/main/java/com/gathergrid/gathergridfeatures/repository/Test/test.java
+++ src/main/java/com/gathergrid/gathergridfeatures/repository/Test/test.java
@@ -1,7 +1,6 @@
 package com.gathergrid.gathergridfeatures.repository.Test;
 
 
-import com.gathergrid.gathergridfeatures.controller.methodes.RegistrationValidation;
 import com.gathergrid.gathergridfeatures.domain.Comment;
 import com.gathergrid.gathergridfeatures.domain.User;
 import com.gathergrid.gathergridfeatures.repository.interfaces.CommentRepositry;
@@ -13,10 +12,7 @@ import java.util.Optional;
 
 public class test {
     public static void main(String[] args) {
-        UserService service =new UserService();
-            String hashed = RegistrationValidation.hashPassword("1234");
-            User newUser = new User("mohammed", "moussafia", "email@gmail.com", hashed);
-            service.save(newUser);
+
 
     }
 }
